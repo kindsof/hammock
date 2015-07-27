2f4fcc2f2e999e8ba1a88d9333b798cd5b92dfd8
Eyal Posener <eyal@stratoscale.com>
2015-07-27 08:55:28 +0300

    move hammock.decorator to hammock.route

--- hammock/resource.py
+++ hammock/resource.py
@@ -1,4 +1,4 @@
-import hammock.decorator as decorator
+import hammock.route as route
 import hammock.common as common
 import collections
 import logging
@@ -6,21 +6,24 @@ import functools
 
 
 TOKEN_ENTRY = "X-Auth-Token"
-CONTENT_TYPE = decorator.CONTENT_TYPE
-TYPE_JSON = decorator.TYPE_JSON
-TYPE_OCTET_STREAM = decorator.TYPE_OCTET_STREAM
-KW_HEADERS = decorator.KW_HEADERS
+CONTENT_TYPE = route.CONTENT_TYPE
+TYPE_JSON = route.TYPE_JSON
+TYPE_OCTET_STREAM = route.TYPE_OCTET_STREAM
+KW_HEADERS = route.KW_HEADERS
 
 
 class Resource(object):
 
     def __init__(self, api, base_path):
         self._api = api
+        self._add_route_methods(base_path)
+
+    def _add_route_methods(self, base_path):
         paths = collections.defaultdict(dict)
-        for method in decorator.iter_route_methods(self):
+        for method in route.iter_route_methods(self):
             paths[method.path]["on_%s" % method.method.lower()] = functools.partial(method.responder, self)
         for path, methods in paths.iteritems():
-            route = type(
+            new_route = type(
                 "_".join([
                     "Resource",
                     base_path.replace("/", "_"),
@@ -28,7 +31,7 @@ class Resource(object):
                     path.translate(None, "{}/."),
                     ]),
                 (), methods)()
-            api.add_route("/%s" % common.url_join(base_path, self.name(), path), route)
+            self._api.add_route("/%s" % common.url_join(base_path, self.name(), path), new_route)
         logging.debug("Added %s to api", self.name())
 
     @classmethod
@@ -37,16 +40,16 @@ class Resource(object):
 
 
 def get(path="", **kwargs):
-    return decorator.route(path, "GET", **kwargs)
+    return route.route(path, "GET", **kwargs)
 
 
 def post(path="", **kwargs):
-    return decorator.route(path, "POST", **kwargs)
+    return route.route(path, "POST", **kwargs)
 
 
 def put(path="", **kwargs):
-    return decorator.route(path, "PUT", **kwargs)
+    return route.route(path, "PUT", **kwargs)
 
 
 def delete(path="", **kwargs):
-    return decorator.route(path, "DELETE", **kwargs)
+    return route.route(path, "DELETE", **kwargs)
