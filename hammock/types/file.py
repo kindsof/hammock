1930272b79ad5d6ec4aa524a86c74163f00a4e70
Eyal Posener <eyal@stratoscale.com>
2015-12-21 16:31:29 +0200

    fix File content-length to be int

--- hammock/types/file.py
+++ hammock/types/file.py
@@ -2,8 +2,8 @@ class File(object):
 
     def __init__(self, stream, content_length):
         self.stream = stream
-        self.content_length = content_length
-        self.len = content_length
+        self.content_length = int(content_length)
+        self.len = self.content_length
 
     def __len__(self):
         return self.content_length
