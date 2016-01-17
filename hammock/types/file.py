899d5e763e59606e39972d5dbcac118045a28304
Eyal Posener <eyal@stratoscale.com>
2016-01-17 11:35:31 +0200

    fix use of client in File type

--- hammock/types/file.py
+++ hammock/types/file.py
@@ -2,6 +2,7 @@ class File(object):
 
     def __init__(self, stream, content_length):
         self.stream = stream
+        self.read = stream.read
         self.content_length = int(content_length)
         self.len = self.content_length
 
