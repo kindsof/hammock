dd2a32f6de7b557e893df50c0ea64f95580ee809
Ravid Brown <ravid.brown@stratoscale.com>
2016-02-28 13:59:54 +0200

    Factory method to create File stream from path to be used with 'with' statement

--- hammock/types/file.py
+++ hammock/types/file.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import
+import os
+
+
 class File(object):
 
     def __init__(self, stream, content_length):
@@ -8,3 +12,14 @@ class File(object):
 
     def __len__(self):
         return self.content_length
+
+
+def from_path(path):
+    f = open(path, 'rb')
+    old_file_position = f.tell()
+    f.seek(0, os.SEEK_END)
+    size = f.tell()
+    f.seek(old_file_position, os.SEEK_SET)
+    file_object = File(f, size)
+    yield file_object
+    file_object.stream.close()
