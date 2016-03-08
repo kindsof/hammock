d3980cdbcdbca2c310fdfc89ffe0b96d127f2062
Eyal Posener <eyal@stratoscale.com>
2016-03-08 13:38:51 +0200

    BK-2337: Init client with session object
    Add option to define headers in CLI

--- hammock/cli/app.py
+++ hammock/cli/app.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 import logging
 import os
+import requests
 import cliff.app as app
 import cliff.interactive as interactive
 import hammock.cli.command_manager as command_manager
@@ -12,6 +13,8 @@ class App(app.App):
     description = 'CLI'
     version = ''
     REQUESTS_LOGGING_LEVEL = logging.ERROR
+    ENV_URL = 'URL'
+    DEFAULT_URL = 'http://localhost'
 
     def __init__(self, clients, **kwargs):
         """
@@ -26,12 +29,18 @@ class App(app.App):
             **kwargs
         )
         self.client_classes = clients
+        self.session = requests.Session()
 
     def build_option_parser(self, description, version, argparse_kwargs=None):
         parser = super(App, self).build_option_parser(description, version, argparse_kwargs)
         parser.add_argument(
             'url', nargs='?',
-            default=os.environ.get('URL', 'http://localhost'),
+            default=os.environ.get(self.ENV_URL, self.DEFAULT_URL),
+            help='Server connection URL.',
+        )
+        parser.add_argument(
+            '--headers', default='',
+            help='Comma separated list of colon separated key-value client headers.'
         )
         return parser
 
@@ -39,7 +48,9 @@ class App(app.App):
         super(App, self).initialize_app(argv)
         logging.getLogger('requests').setLevel(self.REQUESTS_LOGGING_LEVEL if not self.options.debug else logging.DEBUG)
         self.LOG.debug("options: %s", self.options)
-        clients = [client_class(self.options.url) for client_class in self.client_classes]
+        self.options.headers = [header.split(':') for header in self.options.headers.split(',') if ':' in header]
+        self.session.headers.update(dict(self.options.headers))
+        clients = [client_class(self.options.url, session=self.session) for client_class in self.client_classes]
         self.command_manager.load_commands(clients)
 
     def _interactive_app_factory(self, *args, **kwargs):
