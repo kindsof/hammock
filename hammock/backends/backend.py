8deedb3b5083ae1122ff312d216913cff980e2dd
Eyal Safran <eyal.safran@stratoscale.com>
2016-03-17 10:44:25 +0200

    when creating a resource instead of passing None to the 2nd argument, pass the None as a base_path=None

--- hammock/backends/backend.py
+++ hammock/backends/backend.py
@@ -23,7 +23,7 @@ class Backend(object):
             prefix = '/'.join([parent.path for parent in parents])
             node = api.get_node([parent.name for parent in parents])
             node.add(resource_class.name(), resource_class)
-            resource = resource_class(api, None, **resource_params)  # XXX: temporary 2nd argument None, remove them when sagittarius is fixed
+            resource = resource_class(api, base_path=None, **resource_params)  # XXX: temporary 2nd argument None, remove them when sagittarius is fixed
             self._add_route_methods(resource, prefix)
             self._add_sink_methods(resource, prefix)
 
