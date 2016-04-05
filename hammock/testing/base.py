22f711693eb67ab6558112e12006ec3af9e7a7b6
Kfir Gollan <kfir.gollan@stratoscale.com>
2016-04-05 11:03:46 +0300

    Added support for setting extra resource parameters in testing

--- hammock/testing/base.py
+++ hammock/testing/base.py
@@ -20,12 +20,13 @@ def default_404(req, res):  # pylint: disable=unused-argument
 class TestBase(testing.TestBase):
 
     RESOURCES = 'tests.resources'
+    RESOURCE_PARAMS = {}
     POLICY = None
 
     def before(self):
         self.api.add_sink(default_404)
         resources = importlib.import_module(self.RESOURCES)
-        hammock.Hammock(self.api, resources, policy_file=self.POLICY)
+        hammock.Hammock(self.api, resources, policy_file=self.POLICY, **self.RESOURCE_PARAMS)
 
     def _simulate(self, method, url, query_string=None, body=None, headers=None, binary_response=False):
         kwargs = {}
