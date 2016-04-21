f469483d3710e115be0a4d90a6017573fff2e08a
Eyal Posener <eyal@stratoscale.com>
2016-04-21 23:19:07 +0300

    add exceptions to hammock package

--- hammock/__init__.py
+++ hammock/__init__.py
@@ -4,7 +4,8 @@ import hammock.wrappers as wrappers
 from hammock.common import CONTENT_TYPE, CONTENT_LENGTH   # noqa  # pylint: disable=unused-import
 from hammock.common import TYPE_JSON, TYPE_OCTET_STREAM, TOKEN_ENTRY, KW_HEADERS   # noqa  # pylint: disable=unused-import
 from hammock.resource import Resource  # noqa  # pylint: disable=unused-import
-from hammock.api import Hammock  # noqa  # pylint:disable=unused-import
+from hammock.api import Hammock  # noqa  # pylint: disable=unused-import
+import hammock.exceptions as exceptions  # noqa  # pylint: disable=unused-import
 
 
 def get(path='', **kwargs):
