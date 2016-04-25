4b2f0bad90e3ebbbbce1202ce882c86abc429bb3
Eyal Posener <eyal@stratoscale.com>
2016-04-25 17:36:56 +0300

    Remove *_passthrough methods from __init__

--- hammock/__init__.py
+++ hammock/__init__.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import
-import warnings
 import hammock.wrappers as wrappers
 from hammock.common import CONTENT_TYPE, CONTENT_LENGTH   # noqa  # pylint: disable=unused-import
 from hammock.common import TYPE_JSON, TYPE_OCTET_STREAM, TOKEN_ENTRY, KW_HEADERS   # noqa  # pylint: disable=unused-import
@@ -30,26 +29,6 @@ def delete(path='', **kwargs):
 
 def patch(path='', **kwargs):
     return wrappers.route(path, 'PATCH', **kwargs)
-
-
-def get_passthrough(dest, path='', **kwargs):
-    warnings.warn('get_passthrough is deprecated, use get with dest= keyword argument instead', UserWarning)
-    return wrappers.route(path, 'GET', dest=dest, **kwargs)
-
-
-def post_passthrough(dest, path='', **kwargs):
-    warnings.warn('post_passthrough is deprecated, use post with dest= keyword argument instead', UserWarning)
-    return wrappers.route(path, 'POST', dest=dest, **kwargs)
-
-
-def put_passthrough(dest, path='', **kwargs):
-    warnings.warn('put_passthrough is deprecated, use put with dest= keyword argument instead', UserWarning)
-    return wrappers.route(path, 'PUT', dest=dest, **kwargs)
-
-
-def delete_passthrough(dest, path='', **kwargs):
-    warnings.warn('delete_passthrough is deprecated, use delete with dest= keyword argument instead', UserWarning)
-    return wrappers.route(path, 'DELETE', dest=dest, **kwargs)
 
 
 def sink(path='', **kwargs):
