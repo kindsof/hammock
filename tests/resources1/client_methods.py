73b62b24d87df169327a77a078fd3aa9fcc26373
Eyal Posener <eyal@stratoscale.com>
2016-05-24 15:56:49 +0300

    Nicer client_methods example

--- tests/resources1/client_methods.py
+++ tests/resources1/client_methods.py
@@ -2,23 +2,20 @@ from __future__ import absolute_import
 import hammock
 
 
-ALLOWED_ACTIONS = {'run', 'jump'}
-
-
 class ClientMethods(hammock.Resource):
     """
     This class is an example of how to write actions in REST resource.
     """
 
     POLICY_GROUP_NAME = False
-    _specs = None
+    ALLOWED_ACTIONS = {'run', 'jump'}
 
     @hammock.put(
         'action',
         client_methods={name: {'name': name} for name in ALLOWED_ACTIONS},
     )
     def action(self, name, **kwargs):
-        if name not in ALLOWED_ACTIONS:
+        if name not in self.ALLOWED_ACTIONS:
             raise hammock.exceptions.BadRequest('action {} not allowed'.format(name))
         self.check_spec_match(name, kwargs)
         return getattr(self, name)(**kwargs)
