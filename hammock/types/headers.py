f7183c6566ba8521139cfefd185f6a8cd84cd08a
yevgeny shnaidman <yevgeny@stratoscale.com>
2016-07-07 17:56:42 +0300

    converting values of headers to str only in case it is not strting type ( support unicode in headers )

--- hammock/types/headers.py
+++ hammock/types/headers.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import
 import requests.structures as structures
+import six
 
 
 class Headers(structures.CaseInsensitiveDict):
 
     def __setitem__(self, key, value):
-        super(Headers, self).__setitem__(key, str(value))
+        super(Headers, self).__setitem__(key, str(value) if not isinstance(value, six.string_types) else value)
 
     def __call__(self, key):
         return self.get(key)
