074e39f7aed2af07305fc34fa8918325613795fe
Efrat Turgeman <efrat@stratoscale.com>
2016-11-10 09:11:12 +0200

    SYMP-685: Add support for credentials in sink function. As part of SYMP-685, we needed to handle credentials in S3 sink function. This commit add this support.

--- hammock/wrappers/_sink.py
+++ hammock/wrappers/_sink.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 import hammock.proxy as proxy
+import hammock.common as common
 
 from . import wrapper
 
@@ -7,6 +8,10 @@ from . import wrapper
 class Sink(wrapper.Wrapper):
 
     def _wrapper(self, req):
+        if self.credentials_class and common.KW_CREDENTIALS in self.spec.args:
+            credentials = self.credentials_class(req.headers)
+            req.url_params[common.KW_CREDENTIALS] = credentials
+
         if self.dest is None:
             return self(req, **req.url_params)  # pylint: disable=not-callable
         else:
